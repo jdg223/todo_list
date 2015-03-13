ae8efe1bc834fbe58fdca00cba3ab37079701f7a
J.D. Garza <jdgarza22@yahoo.com>
2015-03-13 13:26:45 -0500

    made todo_list app functional

--- todo.php
+++ todo.php
@@ -1,75 +1,179 @@
-Building a Todo List in PHP
-Now that we have a good understanding of arrays, the command line, checking user input, and control structures, it is time to build an application that we'll be revisiting through-out the course.
 
-A Todo List
-
-The Specs
-
-The application should run on command line, should prompt users to add new items, allow them to add text items, and the ability to remove completed items.
-
-After each action, it should display the todo list and the menu.
-
-The Code
-
-Together we've built the basic application with the following code (or very similar):
 
 <?php
 
 // Create array to hold list of todo items
-$items = array();
-
+$items = array('0','1');
+array_unshift($items,"");
+unset($items[0]);
 // The loop!
 do {
+
     // Iterate through list items
     foreach ($items as $key => $item) {
+
         // Display each item and a newline
         echo "[{$key}] {$item}\n";
     }
 
     // Show the menu options
-    echo '(N)ew item, (R)emove item, (Q)uit : ';
+    echo '(N)ew item, (R)emove item, (U)ppercase, (L)owercase, (S)ort, (Q)uit: ';
 
     // Get the input from user
     // Use trim() to remove whitespace and newlines
     $input = trim(fgets(STDIN));
 
     // Check for actionable input
-    if ($input == 'N') {
-        // Ask for entry
-        echo 'Enter item: ';
-        // Add entry to list array
-        $items[] = trim(fgets(STDIN));
-    } elseif ($input == 'R') {
-        // Remove which item?
-        echo 'Enter item number to remove: ';
-        // Get array key
-        $key = trim(fgets(STDIN));
-        // Remove from array
-        unset($items[$key]);
+    switch ($input) {
+      case 'N':
+      // Ask for entry
+      echo 'Enter item: ';
+      // Add entry to list array
+      $items[] = trim(fgets(STDIN));
+      break;
+
+      case 'R':
+      // Remove which item?
+      echo 'Enter item number to remove: ';
+      // Get array key
+      $key = trim(fgets(STDIN));
+      // Remove from array
+      unset($items[$key]);
+      break;
+
+      case 'U':
+      // ask if user wants uppercase values
+      echo "Would you like to make the values uppercase?\nType Y/N: ";
+      //ask user to answer Y/N
+      $answer = trim(fgets(STDIN));
+      if ($answer == 'Y') {
+        $items = array_map('strtoupper',$items);
+      }elseif ($answer == 'N') {
+        null;
+      }
+      break;
+
+      case 'L':
+      // ask if user wants lower case values
+      echo "Would you like to make the values lowercase?\nType Y/N: ";
+      //ask user to answer Y/N
+        $answer = trim(fgets(STDIN));
+        //if Y do the the following
+      if ($answer == 'Y') {
+        $items = array_map('strtolwer',$items);
+      }
+      // if 'N' do the following
+      elseif ($answer == 'N') {
+        null;
+      }
+      break;
+
+      case 'S':
+      sorter($input);
+      break;
+
     }
 // Exit when input is (Q)uit
 } while ($input != 'Q');
 
+
+function sorter($input){
+  foreach ($items as $key => $item) {
+
+  echo '(A)-Z, (Z)-A, (O)rder entered, (Re)verse order entered: ';
+  $input = trim(fgets(STDIN));
+switch ($input) {
+  case 'A':
+  //ask's if user wants to order from A-Z
+  echo "Would you like to order the items in alphabetical order from A-Z?\nType Y/N: ";
+  //ask user to answer Y/N
+    $answer = trim(fgets(STDIN));
+    //if Y do the the following
+    if ($answer == 'Y') {
+      asort($items);
+    }
+    //if 'N' do the following
+    else if($answer == 'N'){
+      null;
+    }
+    break;
+
+  case 'Z':
+  //ask's if user wants to order from Z-A
+  echo "Would you like to order the items in alphabetical order from Z-A?\nType Y/N: ";
+  //ask user to answer Y/N
+    $answer = trim(fgets(STDIN));
+    //if 'Y' do the the following
+    if ($answer == 'Y') {
+      arsort($items);
+    }
+    //if 'N' do the following
+    else if($answer == 'N'){
+      null;
+    }
+    break;
+
+  case 'O':
+  //ask's if user wants to order based on key value from low to high
+  echo "Would you like to order item number's from low to high?\nType Y/N: ";
+  //ask user to answer Y/N
+    $answer = trim(fgets(STDIN));
+    //if 'Y' do the the following
+    if ($answer == 'Y') {
+      ksort($items);
+    }
+    //if 'N' do the following
+    else if($answer == 'N'){
+      null;
+    }
+    break;
+
+  case 'Re':
+  //ask's if user wants to order based on key value from high to low
+  echo "Would you like to order item number's from high to low?\nType Y/N: ";
+  //ask user to answer Y/N
+    $answer = trim(fgets(STDIN));
+    //if 'Y' do the the following
+    if ($answer == 'Y') {
+      krsort($items);
+    }
+    //if 'N' do the following
+    else if($answer == 'N'){
+      null;
+    }
+    break;
+    }
+  }
+}
 // Say Goodbye!
 echo "Goodbye!\n";
 
 // Exit with 0 errors
 exit(0);
 ?>
-Most of this code should be very familiar by now, but we have introduced a few new items.
+<!-- Most of this code should be very familiar by now, but we have introduced a few
+new items.
 
-trim() removed whitespace and special characters from strings. See the PHP man page for trim().
+trim() removed whitespace and special characters from strings. See the PHP man
+page for trim().
 
-We used a do/while instead of another control structure. This allowed us to enter our loop, and pause at the user input area until the user decides to (Q)uit.
+We used a do/while instead of another control structure. This allowed us to
+enter our loop, and pause at the user input area until the user decides to (Q)uit.
 
 We used unset() to remove array elements. See the PHP man page for unset().
 
 Exercises
 
-Create a new directory in your vagrant-lamp directory named todo_list with a file named todo.php containing the code above. Use git init to initialize a new local repository in that directory and commit your code. Create a new remote repository on GitHub called CLI_Todo_List and add the remote to your local repository so you can push your code.
+Create a new directory in your vagrant-lamp directory named todo_list with a
+file named todo.php containing the code above. Use git init to initialize a new
+local repository in that directory and commit your code. Create a new remote
+repository on GitHub called CLI_Todo_List and add the remote to your local
+repository so you can push your code.
 
 After each exercise item, commit and push changes to your GitHub repository.
 
-Update the code to allow upper and lowercase inputs from user for all menu items. Test adding, removing, and quitting.
+Update the code to allow upper and lowercase inputs from user for all menu items.
+Test adding, removing, and quitting.
 
-Update the program to start numbering the list with 1 instead of 0. Make sure remove still works as expected.
+Update the program to start numbering the list with 1 instead of 0. Make sure
+remove still works as expected. -->
